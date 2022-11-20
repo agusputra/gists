cc4fe92eae07c7d734c7a6b1f4eea0d6889e5034
Agus Syahputra <agus.putra99@gmail.com>
2022-11-20 13:23:12 +0700

    Update setup-docker.sh

--- setup-docker.sh
+++ setup-docker.sh
@@ -9,11 +9,9 @@
 set -ex
 export DEBIAN_FRONTEND=noninteractive
 
-# apt remove docker docker-engine docker.io containerd runc
-
 apt update
-apt upgrade
-apt install ca-certificates curl gnupg lsb-release
+apt upgrade -y
+apt install -y ca-certificates curl gnupg lsb-release
 mkdir -p /etc/apt/keyrings
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -23,7 +21,7 @@ echo \
   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt update
-apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 usermod -aG docker ubuntu
 newgrp docker
 systemctl enable docker.service
